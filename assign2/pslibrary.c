1c66f7d9c42b3eb893a0d7db3f51c0b111cffcd4
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2020-10-09 12:26:19 -0500

    Completed working program and tested outputs
    Currently still working on:
    - report.txt; Program output is included, but the writeup is not
    complete. Validating program output is also required.
    - Testing on fox machine; This is necessary to make sure no issues come
    up during grading.

--- assign2/pslibrary.c
+++ assign2/pslibrary.c
@@ -6,10 +6,15 @@ Course/Section: CS 3733 - 002
 Due Date: 11OCT2020
 Instructor: Dr. Silvestro
 */
+#include "pslibrary.h"
+#include <stdio.h>
+
+// Troubleshooting function to print out all arguments passed to the current function
+// printf("%d %d %d %d %d %d\n", x1, y1, z1, x2, y2, z2);
 
 // Array to translate from ready states to letters
 static char stateChars[] = {'r', 'R', 'w', 0};
-/*
+
 void fcfs(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
 {
 
@@ -32,6 +37,257 @@ void fcfs(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
     int cpuLeft2 = x2; // P2 next CPU burst remaining
     int ioLeft1 = y1;  // P1 next IO burst remaining, 0 if no more IO
     int ioLeft2 = y2;  // P2 next IO burst remaining, 0 if no more IO
+
+    for (i = 0; (state1 != DONE) || (state2 != DONE); i++) {
+        // running process completes its CPU burst
+        if ((state1 == RUNNING) && (cpuLeft1 == 0)) {
+            if (ioLeft1 == 0) {
+                state1 = DONE;
+                s1[i] = stateChars[state1];
+                s1[i + 1] = '\0';
+            }
+            else
+                state1 = WAITING;
+        }
+        else if ((state2 == RUNNING) && (cpuLeft2 == 0)) {
+            if (ioLeft2 == 0) {
+                state2 = DONE;
+                s2[i] = stateChars[state2];
+                s2[i + 1] = '\0';
+            }
+            else
+                state2 = WAITING;
+        }
+        // handle IO complete
+        if ((state1 == WAITING) && (ioLeft1 == 0)) {
+            state1 = READY;
+            cpuLeft1 = z1;
+        }
+        if ((state2 == WAITING) && (ioLeft2 == 0)) {
+            state2 = READY;
+            cpuLeft2 = z2;
+        }
+        // if both ready, depends on algorithm
+        if ((state1 == READY) && (state2 == READY)) {
+            state1 = RUNNING;
+        }
+        // handle one ready and CPU available
+        else if ((state1 == READY) && (state2 != RUNNING)) {
+            state1 = RUNNING;
+        }
+        else if ((state2 == READY) && (state1 != RUNNING)) {
+            state2 = RUNNING;
+        }
+        // insert chars in string, but avoid putting in extra string terminators
+        if (state1 != DONE)
+            s1[i] = stateChars[state1];
+        if (state2 != DONE)
+            s2[i] = stateChars[state2];
+        // decrement counts
+        // OK to decrement even if nothing running
+        if (state1 == RUNNING)
+            cpuLeft1--;
+        if (state1 == WAITING)
+            ioLeft1--;
+        if (state2 == RUNNING)
+            cpuLeft2--;
+        if (state2 == WAITING)
+            ioLeft2--;
+    } // end of main for loop
+}
+
+void sjf(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    // 1) handle state changes:
+    //         running process completes CPU burst
+    //         running process has quantum expire
+    //         IO complete
+    //   2) do context switch if necessary
+    //         both ready
+    //         one ready and CPU free
+    //   3) append appropriate characters to character arrays
+    //         avoid putting in multiple string terminators
+    int i;              // next string position (time)
+    int state1 = READY; // start with both ready
+    int state2 = READY;
+    int cpuLeft1 = x1; // P1 next CPU burst remaining
+    int cpuLeft2 = x2; // P2 next CPU burst remaining
+    int ioLeft1 = y1;  // P1 next IO burst remaining, 0 if no more IO
+    int ioLeft2 = y2;  // P2 next IO burst remaining, 0 if no more IO
+
+    for (i = 0; (state1 != DONE) || (state2 != DONE); i++) {
+        // running process completes its CPU burst
+        if ((state1 == RUNNING) && (cpuLeft1 == 0)) {
+            if (ioLeft1 == 0) {
+                state1 = DONE;
+                s1[i] = stateChars[state1];
+                s1[i + 1] = '\0';
+            }
+            else
+                state1 = WAITING;
+        }
+        else if ((state2 == RUNNING) && (cpuLeft2 == 0)) {
+            if (ioLeft2 == 0) {
+                state2 = DONE;
+                s2[i] = stateChars[state2];
+                s2[i + 1] = '\0';
+            }
+            else
+                state2 = WAITING;
+        }
+        // handle IO complete
+        if ((state1 == WAITING) && (ioLeft1 == 0)) {
+            state1 = READY;
+            cpuLeft1 = z1;
+        }
+        if ((state2 == WAITING) && (ioLeft2 == 0)) {
+            state2 = READY;
+            cpuLeft2 = z2;
+        }
+        // if both ready, depends on algorithm
+        if ((state1 == READY) && (state2 == READY)) {
+            if (cpuLeft1 <= cpuLeft2) {
+                state1 = RUNNING;
+            }
+            else {
+                state2 = RUNNING;
+            }
+        }
+        // handle one ready and CPU available
+        else if ((state1 == READY) && (state2 != RUNNING)) {
+            state1 = RUNNING;
+        }
+        else if ((state2 == READY) && (state1 != RUNNING)) {
+            state2 = RUNNING;
+        }
+        // insert chars in string, but avoid putting in extra string terminators
+        if (state1 != DONE)
+            s1[i] = stateChars[state1];
+        if (state2 != DONE)
+            s2[i] = stateChars[state2];
+        // decrement counts
+        // OK to decrement even if nothing running
+        if (state1 == RUNNING)
+            cpuLeft1--;
+        if (state1 == WAITING)
+            ioLeft1--;
+        if (state2 == RUNNING)
+            cpuLeft2--;
+        if (state2 == WAITING)
+            ioLeft2--;
+    } // end of main for loop
+}
+void psjf(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    // 1) handle state changes:
+    //         running process completes CPU burst
+    //         running process has quantum expire
+    //         IO complete
+    //   2) do context switch if necessary
+    //         both ready
+    //         one ready and CPU free
+    //   3) append appropriate characters to character arrays
+    //         avoid putting in multiple string terminators
+
+    int i;              // next string position (time)
+    int state1 = READY; // start with both ready
+    int state2 = READY;
+    int cpuLeft1 = x1; // P1 next CPU burst remaining
+    int cpuLeft2 = x2; // P2 next CPU burst remaining
+    int ioLeft1 = y1;  // P1 next IO burst remaining, 0 if no more IO
+    int ioLeft2 = y2;  // P2 next IO burst remaining, 0 if no more IO
+
+    for (i = 0; (state1 != DONE) || (state2 != DONE); i++) {
+        // running process completes its CPU burst
+        if ((state1 == RUNNING) && (cpuLeft1 == 0)) {
+            if (ioLeft1 == 0) {
+                state1 = DONE;
+                s1[i] = stateChars[state1];
+                s1[i + 1] = '\0';
+            }
+            else
+                state1 = WAITING;
+        }
+        else if ((state2 == RUNNING) && (cpuLeft2 == 0)) {
+            if (ioLeft2 == 0) {
+                state2 = DONE;
+                s2[i] = stateChars[state2];
+                s2[i + 1] = '\0';
+            }
+            else
+                state2 = WAITING;
+        }
+        // handle IO complete
+        if ((state1 == WAITING) && (ioLeft1 == 0)) {
+            state1 = READY;
+            cpuLeft1 = z1;
+        }
+        if ((state2 == WAITING) && (ioLeft2 == 0)) {
+            state2 = READY;
+            cpuLeft2 = z2;
+        }
+        // if both ready, depends on algorithm
+        if ((state1 == READY) && (state2 == READY)) {
+            if (cpuLeft1 <= cpuLeft2) {
+                state1 = RUNNING;
+            }
+            else {
+                state2 = RUNNING;
+            }
+        }
+        // handle one ready and CPU available
+        else if ((state1 == READY) && (state2 != RUNNING)) {
+            state1 = RUNNING;
+        }
+        else if ((state2 == READY) && (state1 != RUNNING)) {
+            state2 = RUNNING;
+        }
+        else if ((state1 == READY) && (cpuLeft1 < cpuLeft2)) {
+            state2 = READY;
+            state1 = RUNNING;
+        }
+        else if ((state2 == READY) && (cpuLeft2 < cpuLeft1)) {
+            state1 = READY;
+            state2 = RUNNING;
+        }
+
+        // insert chars in string, but avoid putting in extra string terminators
+        if (state1 != DONE)
+            s1[i] = stateChars[state1];
+        if (state2 != DONE)
+            s2[i] = stateChars[state2];
+        // decrement counts
+        // OK to decrement even if nothing running
+        if (state1 == RUNNING)
+            cpuLeft1--;
+        if (state1 == WAITING)
+            ioLeft1--;
+        if (state2 == RUNNING)
+            cpuLeft2--;
+        if (state2 == WAITING)
+            ioLeft2--;
+    } // end of main for loop
+}
+
+void rr(char *s1, char *s2, int quantum, int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    // 1) handle state changes:
+    //         running process completes CPU burst
+    //         running process has quantum expire
+    //         IO complete
+    //   2) do context switch if necessary
+    //         both ready
+    //         one ready and CPU free
+    //   3) append appropriate characters to character arrays
+    //         avoid putting in multiple string terminators
+
+    int i;              // next string position (time)
+    int state1 = READY; // start with both ready
+    int state2 = READY;
+    int cpuLeft1 = x1; // P1 next CPU burst remaining
+    int cpuLeft2 = x2; // P2 next CPU burst remaining
+    int ioLeft1 = y1;  // P1 next IO burst remaining, 0 if no more IO
+    int ioLeft2 = y2;  // P2 next IO burst remaining, 0 if no more IO
     int qleft;         // quantum remaining
 
     for (i = 0; (state1 != DONE) || (state2 != DONE); i++) {
@@ -46,14 +302,28 @@ void fcfs(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
                 state1 = WAITING;
         }
         else if ((state2 == RUNNING) && (cpuLeft2 == 0)) {
-            ...
+            if (ioLeft2 == 0) {
+                state2 = DONE;
+                s2[i] = stateChars[state2];
+                // terminate the string
+            }
+            else
+                state2 = WAITING;
         }
         // running process has quantum expire
         if ((state1 == RUNNING) && (qleft == 0)) {
-            ...
+            state1 = READY;
+            if (state2 == READY) {
+                state2 = RUNNING;
+                qleft = quantum;
+            }
         }
         if ((state2 == RUNNING) && (qleft == 0)) {
-            ...
+            state2 = READY;
+            if (state1 == READY) {
+                state1 = RUNNING;
+                qleft = quantum;
+            }
         }
         // handle IO complete
         if ((state1 == WAITING) && (ioLeft1 == 0)) {
@@ -61,11 +331,13 @@ void fcfs(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
             cpuLeft1 = z1;
         }
         if ((state2 == WAITING) && (ioLeft2 == 0)) {
-            ...
+            state2 = READY;
+            cpuLeft2 = z2;
         }
         // if both ready, depends on algorithm
         if ((state1 == READY) && (state2 == READY)) {
-            ...
+            state1 = RUNNING;
+            qleft = quantum;
         }
         // handle one ready and CPU available
         else if ((state1 == READY) && (state2 != RUNNING)) {
@@ -73,7 +345,8 @@ void fcfs(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
             qleft = quantum;
         }
         else if ((state2 == READY) && (state1 != RUNNING)) {
-            ...
+            state2 = RUNNING;
+            qleft = quantum;
         }
         // insert chars in string, but avoid putting in extra string terminators
         if (state1 != DONE)
@@ -81,8 +354,7 @@ void fcfs(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
         if (state2 != DONE)
             s2[i] = stateChars[state2];
         // decrement counts
-        qleft--;
-        // OK to decrement even if nothing running
+        qleft--; // OK to decrement even if nothing running
         if (state1 == RUNNING)
             cpuLeft1--;
         if (state1 == WAITING)
@@ -93,4 +365,3 @@ void fcfs(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2)
             ioLeft2--;
     } // end of main for loop
 }
-}*/
