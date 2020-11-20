8093bf79b945f86da4f8352d030c514629d393b0
Andrew DiCarlo <andrew.dicarlo@gmail.com>
2020-11-19 20:52:04 -0600

    Initial part1 commit

--- assign4/assign4-part1.c
+++ assign4/assign4-part1.c
@@ -7,3 +7,17 @@ Due Date: 29NOV2020
 Instructor: Dr. Silvestro
 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv])
+{
+    if (argc != 2) {
+        printf("Usage:\n\t%s nthreads\n\tModels the dining philsophers problem with nthreads number of philosphers",
+               argv[0]);
+        exit(1);
+    }
+
+    return 0;
+}
